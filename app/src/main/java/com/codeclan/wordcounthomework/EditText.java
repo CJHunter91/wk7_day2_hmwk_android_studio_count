4b2e50c98b8efd03b79b44d78d84cf08764551d3
Chris Hunter <orkem2@gmail.com>
2017-08-08 15:09:56 +0100

    initial commit

---
+++ app/src/main/java/com/codeclan/wordcounthomework/EditText.java
@@ -0,0 +1,13 @@
+package com.codeclan.wordcounthomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+    }
+}
---
+++ app/src/test/java/com/codeclan/wordcounthomework/WordCountTest.java
@@ -0,0 +1,8 @@
+package com.codeclan.wordcounthomework;
+
+/**
+ * Created by user on 08/08/2017.
+ */
+
+public class WordCountTest {
+}
